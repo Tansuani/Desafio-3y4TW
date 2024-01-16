4641ba0876155affd728b058512bf992230285d8
Tansuani Waring <tansuanigomezw@gmail.com>
2024-01-16 20:41:49 -0300

    managing packages

--- consultas.js
+++ consultas.js
@@ -1,14 +1,15 @@
 import pg from 'pg'
 
 const pool = new pg.Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Amara01.',
-    database: 'likeme',
-    allowExitOnIdle:true,
+    host: "localhost",
+    user: "postgres",
+    password: "Amara01.",
+    database: "likeme",
+    allowExitOnIdle: true
 })
+
 const getDate = async () => {
     const result = await pool.query("SELECT NOW()")
-    console.log(result)
+    Console.log(result)
 }
 getDate()
